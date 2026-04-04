df0966ec6bb89a28fceae0c442b72a90ee1c58d9
Kishore <Kishore062922@gmail.com>
2026-04-04 12:11:50 +0530

    added real cors

--- backend/src/main/java/com/franchise/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/franchise/backend/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
     @Bean
         public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173","https://franchise-analytics-platform.onrender.com"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://franchise-analytics-platform.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
