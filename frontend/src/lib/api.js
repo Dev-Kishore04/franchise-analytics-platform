7ed1aee2b0779142f157b23d9e1309064c26337d
Kishore <Kishore062922@gmail.com>
2026-04-04 11:15:15 +0530

    set production api url

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,8 +1,8 @@
 // src/lib/api.js
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080'
-
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://franchise-analytics-platform.onrender.com'
+console.log("API URL:", BASE_URL)
 const api = axios.create({
   baseURL: BASE_URL,
   headers: { 'Content-Type': 'application/json' },
